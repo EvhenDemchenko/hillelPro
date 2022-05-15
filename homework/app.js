6b47fb538e27a3be40acea363f6fb16aa5d41a08
Yevhen Demchenko <yevhendemchenko@MacBook-Air-Yevhen.local>
2022-05-15 14:26:55 +0300

    add new fix)

--- homework/app.js
+++ homework/app.js
@@ -1,13 +1,13 @@
 document.querySelector('.btn1').addEventListener('click', () => {
     console.log(taskOne())
 });
-
+// первый вариант
 function taskOne() {
     let inputValue;
     for (let i = 0; i < 10; i++) {
         inputValue = prompt('enter value > 100');
         if (inputValue > 100) {
-            return inputValue;
+            return +inputValue;
         } else if (inputValue < 100) {
             if (i === 9) {
                 return inputValue;
@@ -19,4 +19,15 @@ function taskOne() {
         }
     }
 }
+// второй вариант
+function taskOne1() {
+    let inputValue;
+    for (let i = 0; i < 10; i++) {
+        inputValue = prompt('enter value > 100');
+        if (inputValue > 100 || i === 9 || isNaN(inputValue)) {
+            return inputValue
+        }
+    }
+}
+
 
