ea4321d5afb4c9956af8894e76b10fa4166f4622
Yevhen Demchenko <yevhendemchenko@MacBook-Air-Yevhen.local>
2022-06-13 22:22:29 +0300

    add some fixes

--- homework11/app.js
+++ homework11/app.js
@@ -2,15 +2,11 @@ const input = document.querySelector('input');
 const list = document.querySelectorAll('li');
 
 input.addEventListener('input', (event) => {
-    (event.target.value.match(/[A-Z А-Я]/)) ? list[0].style.background = ('red') : list[0].style.background = ('transparent');
+    (event.target.value.match(/[A-ZА-Я]/)) ? list[0].style.background = ('red') : list[0].style.background = ('transparent');
     (event.target.value.match(/\d/)) ? list[1].style.background = ('red') : list[1].style.background = ('transparent');
-    (event.target.value.match(/\W/)) ? list[2].style.background = ('red') : list[2].style.background = ('transparent');
+    (event.target.value.match(/[^\d\sA-ZА-Я]/i)) ? list[2].style.background = ('red') : list[2].style.background = ('transparent');
     (event.target.value.match(/\S{8}/)) ? list[3].style.background = ('red') : list[3].style.background = ('transparent');
-})
-
-
-
-
+});
 
 
 
