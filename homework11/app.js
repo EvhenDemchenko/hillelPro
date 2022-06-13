99e089ab68b8e854f552211bd18620f1c2f557e9
Yevhen Demchenko <yevhendemchenko@MacBook-Air-Yevhen.local>
2022-06-13 19:27:58 +0300

    add fix

--- homework11/app.js
+++ homework11/app.js
@@ -1,12 +1,12 @@
-    const input = document.querySelector('input');
-    const list  = document.querySelectorAll('li');
+const input = document.querySelector('input');
+const list = document.querySelectorAll('li');
 
-    input.addEventListener('input', (event)=>{
-        (event.target.value.match(/[A-Z]/)) ? list[0].style.background=('red') : list[0].style.background=('transparent');
-        (event.target.value.match(/\d/)) ? list[1].style.background=('red') : list[1].style.background=('transparent');
-        (event.target.value.match(/\W/)) ? list[2].style.background=('red') : list[2].style.background=('transparent');
-        (event.target.value.match(/\S{8}/)) ? list[3].style.background=('red') : list[3].style.background=('transparent');
-    })
+input.addEventListener('input', (event) => {
+    (event.target.value.match(/[A-Z А-Я]/)) ? list[0].style.background = ('red') : list[0].style.background = ('transparent');
+    (event.target.value.match(/\d/)) ? list[1].style.background = ('red') : list[1].style.background = ('transparent');
+    (event.target.value.match(/\W/)) ? list[2].style.background = ('red') : list[2].style.background = ('transparent');
+    (event.target.value.match(/\S{8}/)) ? list[3].style.background = ('red') : list[3].style.background = ('transparent');
+})
 
 
 
