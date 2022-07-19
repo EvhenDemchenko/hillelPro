ea8e115ad9da7f5cf32c0d5e5225b79b2f3eead5
Yevhen Demchenko <yevhendemchenko@MacBook-Air-Yevhen.local>
2022-07-19 22:42:28 +0300

    add webpack

---
+++ homework16/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: [
+            path.join(__dirname, '/node_modules/bootstrap/dist/css/bootstrap.min.css'),
+            path.join(__dirname, './src/main.js'),
+            path.join(__dirname, './src/style.scss'),
+        ]
+    },
+    devtool: 'source-map',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            }
+        ]
+    }
+}
